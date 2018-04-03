a959082eab07691bdf2b0b948fb5127db3d5f1dc
thomasnilsson <s144470@student.dtu.dk>
2018-04-03 22:33:53 +0200

    Wow much viz

---
+++ js/crashes.js
@@ -0,0 +1,130 @@
+(function() {
+	var h = 300, w = 1200,
+		margins = {bottom: h - 50, top: 20, left: 50, right: w - 150},
+		animationTime = 1000,
+		body = d3.select("body"),
+		div = body.select("#crashPlot"),
+		tooltip = d3.select("#tooltipCrashes")
+			.classed("hidden", true),
+		title = div.append("h4"),
+		svg = div.append("svg")
+			.attr("width", w)
+			.attr("height", h),
+		normalColor = "#52707C",
+		hoverColor = "#FFA500"
+		
+
+	var parseRow = row => {
+	  return {
+	    "version" : row.version,
+	    "counts" : +row.counts
+	  }
+	}
+
+	var innerPadding = 0.5, categoryIndex = 0,
+		x, y, xAxis, yAxis, months, dataset
+
+	var handleMouseOver = (rect, d) => {
+		// Use mouse coordinates for tooltip position
+		var xPos = d3.event.pageX
+		var yPos = d3.event.pageY
+
+		// Update the tooltip position
+		tooltip.style("left", xPos + "px").style("top", yPos + "px")
+
+		// Update the tooltip information
+		d3.select("#version")
+			.text(d.version)
+
+		d3.select("#crashes")
+			.text(d.counts)
+
+		// Show the tooltip
+		tooltip.classed("hidden", false)
+
+		// Highlight the current bar
+		d3.select(rect).attr("fill", hoverColor)
+	}
+
+	var handleMouseOut = rect => {
+		//Hide the tooltip again
+		tooltip.classed("hidden", true)
+
+		// Remove highlight from the current bar
+		d3.select(rect)
+			.transition()
+			.duration(250)
+			.attr("fill", normalColor)
+	}
+
+	d3.csv("data/crashes.csv", parseRow, data => {
+
+		console.log(data)
+		// Find highest y value
+		var yMax = d3.max(data, d => d.counts)
+
+		// Find the x domain, the result here is: [Jan, Feb, Mar... Dec]
+		var versions = data.map(d => d.version)
+
+		// x Scale
+		x = d3.scaleBand()
+			.domain(versions)
+			.rangeRound([margins.left, margins.right])
+			.paddingInner(innerPadding)
+
+		xAxis = d3.axisBottom(x)
+
+		// y Scale
+		y = d3.scaleLinear()
+		.domain([0, yMax])
+		.range([margins.bottom, margins.top])
+
+		yAxis = d3.axisLeft(y).ticks(5)
+
+		// Bars
+		svg.selectAll("rect")
+		.data(data)
+		.enter()
+		.append("rect")
+		.attr("x", d => x(d.version))
+		.attr("y", d =>  y(d.counts))
+		.attr("width", x.bandwidth())
+		.attr("height", d => margins.bottom - y(d.counts))
+		.attr("fill", normalColor)
+			.on("mouseover", function(d) {
+				handleMouseOver(this, d)
+			})
+			.on("mouseout", function() {
+				handleMouseOut(this)
+			})
+
+		// Make x axis with a g-element
+		svg.append("g")
+		  .attr("transform", "translate(0, " + (margins.bottom) + ")")
+		  .call(xAxis)
+
+		// Make y axis with another g-element
+		svg.append("g")
+		  .attr("id", "yAxis")
+		  .attr("transform", "translate(" + margins.left + ", 0)")
+		  .call(yAxis)
+
+		// Text label for the y axis
+		svg.append("text")
+			.attr("transform", "rotate(-90)")
+			.style("text-anchor", "middle")
+			.attr("y", margins.left/2 - 10)
+			.attr("x", -h/2)
+			.text("# Update Failure Reports")
+
+		// Text label for the y axis
+		svg.append("text")
+			// .attr("transform", "rotate(-90)")
+			.style("text-anchor", "middle")
+			.attr("y", margins.bottom + 40)
+			.attr("x", w/2)
+			.text("Firmware Version")
+
+	})
+
+})()
--- js/noise.js
+++ js/noise.js
@@ -1,217 +1,226 @@
-(function() {
-var w = 900
-var h = 400
+(() => run())()
 
-var w2 = 900
-var h2 = 250
-var boundaries = {bottom: h2 - 60, top: 20, left: 60, right: w2 - 20}
-var innerPadding = 0.1
+function run() {
+	var w = 1200
+	var h = 600
 
-var rand = Math.random()
+	var w2 = 1200
+	var h2 = 250
+	var boundaries = {
+		bottom: h2 - 60,
+		top: 20,
+		left: 60,
+		right: w2 - 20
+	}
+	var innerPadding = 0.1
 
-//Define quantize scale to sort data values into buckets of color
-let color = ['#27586B', '#003549', '#05425A', '#52707C', '#888B8C']
+	var rand = Math.random()
 
-//For converting Dates to strings
-var hourOfDay = time => {
-	var values = time.split(":")
-	return +values[0]
-}
-
-var parseRow = row => ({
-	"hour" : hourOfDay(row.time),
-	"lat": parseFloat(row.lat),
-	"lon": parseFloat(row.long)
-})
-
-//Define map projection
-// From http://bl.ocks.org/phil-pedruco/6646844
-var projection = d3.geoMercator()
-	.center([-73.94, 40.70])
-		.translate([w/2, h/2])
-   	.scale(40 * 1000)
-
-//Define path generator
-var path = d3.geoPath()
-				 .projection(projection)
-
-//Create SVG element
-var svgGeo = d3.select("body").select("#chart1")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h)
-
-var svgHist = d3.select("body").select("#chart2")
-			.append("svg")
-			.attr("width", w2)
-			.attr("height", h2)
-
-//Load in GeoJSON data
-d3.json("data/boroughs.geojson", (error, json) => {
-	if(error) console.log("error fetching data")
-
-	//Bind data and create one path per GeoJSON feature
-	svgGeo.selectAll("path")
-	   .data(json.features)
-	   .enter()
-	   .append("path")
-	   .attr("d", path)
-	   .style("fill", (d,i) => color[i])
-	   .style("stroke", "black")
-	   .style("stroke-width", "0.5")
-
-	   svgGeo.selectAll("text")
-		  .data(json.features)
-		  .enter()
-		  .append("text")
-		  .attr("class", "label")
-		  .attr("x", d => path.centroid(d)[0] - 30)
-		  .attr("y", d => path.centroid(d)[1])
-		  .style("fill", "black")
-		  .text(d => d.properties.BoroName)
-
-	d3.csv("data/murders2016.csv", parseRow, data => {
-
-		var hours = d3.range(24)
-		var counts = hours.map(x => 0) // 24x0 array
-
-		for (i in data) {
-			let hour = data[i].hour
-			counts[hour] += 1
-		}
-
-		let n = data.length > 0 ? data.length : 1
-		counts = counts.map(c => (c / n) * 500 * rand)
-
-		x = d3.scaleBand()
-		  .domain(hours)
-		  .rangeRound([boundaries.left, boundaries.right])
-		  .paddingInner(innerPadding)
-
-		xAxis = d3.axisBottom(x)
-
-		y = d3.scaleLinear()
-		  .domain([0, d3.max(counts)])
-		  .range([boundaries.bottom, boundaries.top])
-
-		yAxis = d3.axisLeft(y).ticks(5)
-
-		// Make x axis with a g-element
-		svgHist.append("g")
-			.attr("transform", "translate(0, " + (boundaries.bottom) + ")")
-			.call(xAxis)
-
-		// Make y axis with another g-element
-		svgHist.append("g")
-			.attr("id", "yAxis")
-			.attr("transform", "translate(" + boundaries.left + ", 0)")
-			.call(yAxis)
-
-		// Text label for the y axis
-		svgHist.append("text")
-			.attr("transform", "rotate(-90)")
-			.style("text-anchor", "middle")
-			.attr("y", boundaries.left/2 - 10)
-			.attr("x", -h2/2)
-			.text("Average Noise Level (dB)")
+	//Define quantize scale to sort data values into buckets of color
+	let color = ['#27586B', '#003549', '#05425A', '#52707C', '#888B8C']
+	let oticonPink = "#b6007a"
 
 
-		// Text label for the y axis
-		svgHist.append("text")
-			.style("text-anchor", "middle")
-			.attr("y", boundaries.bottom + 40)
-			.attr("x", w2/2)
-			.text("Hour of Day")
+	//For converting Dates to strings
+	var hourOfDay = time => {
+		var values = time.split(":")
+		return +values[0]
+	}
 
+	var parseRow = row => ({
+		"hour": hourOfDay(row.time),
+		"lat": parseFloat(row.lat),
+		"lon": parseFloat(row.long)
+	})
 
-		// Bars
-		svgHist.selectAll("rect")
-			.data(counts)
+	//Define map projection
+	// From http://bl.ocks.org/phil-pedruco/6646844
+	var projection = d3.geoMercator()
+		.center([-73.94, 40.70])
+		.translate([w / 2, h / 2])
+		.scale(58 * 1000)
+
+	//Define path generator
+	var path = d3.geoPath()
+		.projection(projection)
+
+	//Create SVG element
+	var svgGeo = d3.select("body").select("#chart1")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h)
+
+	var svgHist = d3.select("body").select("#chart2")
+		.append("svg")
+		.attr("width", w2)
+		.attr("height", h2)
+
+	//Load in GeoJSON data
+	d3.json("data/boroughs.geojson", (error, json) => {
+		if (error) console.log("error fetching data")
+
+		//Bind data and create one path per GeoJSON feature
+		svgGeo.selectAll("path")
+			.data(json.features)
 			.enter()
-			.append("rect")
-			.attr("x", (d,i) => x(i))
-			.attr("y", d => y(d))
-			.attr("width", x.bandwidth())
-			.attr("height", d => boundaries.bottom - y(d))
-			.attr("fill", "#b6007a")
+			.append("path")
+			.attr("d", path)
+			.style("fill", (d, i) => color[i])
+			.style("stroke", "black")
+			.style("stroke-width", "0.5")
 
-		// DRAW POINTS ON MAP
-		var circles = svgGeo.append("g")
-			.selectAll("circle")
-			.data(data)
+		svgGeo.selectAll("text")
+			.data(json.features)
 			.enter()
-			.append("circle")
-			.attr("cx", d => projection([d.lon, d.lat])[0] )
-			.attr("cy", d => projection([d.lon, d.lat])[1] )
-			.attr("r", 2)
-			.style("stroke-width", 0.25)
-			.style("opacity", 0.75)
-			.attr("class", "non_brushed")
+			.append("text")
+			.attr("class", "label")
+			.attr("x", d => path.centroid(d)[0] - 30)
+			.attr("y", d => path.centroid(d)[1])
+			.style("fill", "black")
+			.text(d => d.properties.BoroName)
 
-		function isBrushed(brush_coords, cx, cy) {
-		     var x0 = brush_coords[0][0],
-		         x1 = brush_coords[1][0],
-		         y0 = brush_coords[0][1],
-		         y1 = brush_coords[1][1]
+		d3.csv("data/noise.csv", parseRow, data => {
 
-		    return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1
-		}
+			var hours = d3.range(24)
+			var counts = hours.map(x => 0) // 24x0 array
 
-		function highlightBrushedCircles() {
-			if (d3.event.selection != null) {
-				circles.attr("class", "non_brushed")
-
-				var brush_coords = d3.brushSelection(this)
-
-				circles.filter(function (){
-						   var cx = d3.select(this).attr("cx"),
-							   cy = d3.select(this).attr("cy")
-
-						   return isBrushed(brush_coords, cx, cy)
-					   })
-					   .attr("class", "brushed")
-			}
-		}
-
-		function updatesvgHistogram(){
-			var dataBrushed =  d3.selectAll(".brushed").data()
-
-			counts = hours.map(x => 0) // 24x0 array
-
-			for (i in dataBrushed) {
-				let hour = dataBrushed[i].hour
+			for (i in data) {
+				let hour = data[i].hour
 				counts[hour] += 1
 			}
 
-			let n = dataBrushed.length > 0 ? dataBrushed.length : 1
-
+			let n = data.length > 0 ? data.length : 1
 			counts = counts.map(c => (c / n) * 500 * rand)
 
-			y.domain([0, d3.max(counts)])
+			x = d3.scaleBand()
+				.domain(hours)
+				.rangeRound([boundaries.left, boundaries.right])
+				.paddingInner(innerPadding)
+
+			xAxis = d3.axisBottom(x)
+
+			y = d3.scaleLinear()
+				.domain([0, d3.max(counts)])
+				.range([boundaries.bottom, boundaries.top])
+
 			yAxis = d3.axisLeft(y).ticks(5)
 
-			svgHist.selectAll("#yAxis")
-				.transition()
+			// Make x axis with a g-element
+			svgHist.append("g")
+				.attr("transform", "translate(0, " + (boundaries.bottom) + ")")
+				.call(xAxis)
+
+			// Make y axis with another g-element
+			svgHist.append("g")
+				.attr("id", "yAxis")
+				.attr("transform", "translate(" + boundaries.left + ", 0)")
 				.call(yAxis)
 
+			// Text label for the y axis
+			svgHist.append("text")
+				.attr("transform", "rotate(-90)")
+				.style("text-anchor", "middle")
+				.attr("y", boundaries.left / 2 - 10)
+				.attr("x", -h2 / 2)
+				.text("Average Noise Level (dB)")
+
+
+			// Text label for the y axis
+			svgHist.append("text")
+				.style("text-anchor", "middle")
+				.attr("y", boundaries.bottom + 40)
+				.attr("x", w2 / 2)
+				.text("Hour of Day")
+
+
+			// Bars
 			svgHist.selectAll("rect")
 				.data(counts)
-				.transition()
-				.attr("x", (d,i) => x(i))
-				.attr("y", d =>  y(d))
+				.enter()
+				.append("rect")
+				.attr("x", (d, i) => x(i))
+				.attr("y", d => y(d))
 				.attr("width", x.bandwidth())
 				.attr("height", d => boundaries.bottom - y(d))
+				.attr("fill", oticonPink)
 
-		}
+			// DRAW POINTS ON MAP
+			var circles = svgGeo.append("g")
+				.selectAll("circle")
+				.data(data)
+				.enter()
+				.append("circle")
+				.attr("cx", d => projection([d.lon, d.lat])[0])
+				.attr("cy", d => projection([d.lon, d.lat])[1])
+				.attr("r", 2)
+				.style("stroke-width", 0.25)
+				.style("opacity", 0.75)
+				.attr("class", "non_brushed")
 
-		var brush = d3.brush()
-		   .on("brush", highlightBrushedCircles)
-		   .on("end", updatesvgHistogram)
+			function isBrushed(brush_coords, cx, cy) {
+				var x0 = brush_coords[0][0],
+					x1 = brush_coords[1][0],
+					y0 = brush_coords[0][1],
+					y1 = brush_coords[1][1]
 
-		svgGeo.append("g")
-			.call(brush)
+				return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1
+			}
+
+			function highlightBrushedCircles() {
+				if (d3.event.selection != null) {
+					circles.attr("class", "non_brushed")
+
+					var brush_coords = d3.brushSelection(this)
+
+					circles.filter(function () {
+							var cx = d3.select(this).attr("cx"),
+								cy = d3.select(this).attr("cy")
+
+							return isBrushed(brush_coords, cx, cy)
+						})
+						.attr("class", "brushed")
+				}
+			}
+
+			function updatesvgHistogram() {
+				var dataBrushed = d3.selectAll(".brushed").data()
+
+				counts = hours.map(x => 0) // 24x0 array
+
+				for (i in dataBrushed) {
+					let hour = dataBrushed[i].hour
+					counts[hour] += 1
+				}
+
+				let n = dataBrushed.length > 0 ? dataBrushed.length : 1
+
+				counts = counts.map(c => (c / n) * 500 * rand)
+
+				y.domain([0, d3.max(counts)])
+				yAxis = d3.axisLeft(y).ticks(5)
+
+				svgHist.selectAll("#yAxis")
+					.transition()
+					.call(yAxis)
+
+				svgHist.selectAll("rect")
+					.data(counts)
+					.transition()
+					.attr("x", (d, i) => x(i))
+					.attr("y", d => y(d))
+					.attr("width", x.bandwidth())
+					.attr("height", d => boundaries.bottom - y(d))
+
+			}
+
+			var brush = d3.brush()
+				.on("brush", highlightBrushedCircles)
+				.on("end", updatesvgHistogram)
+
+			svgGeo.append("g")
+				.call(brush)
+		})
+
+
 	})
-
-
-})
-})()
+}
